41fe224d8177460820ee0bff6549f496ea9ba439
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-11-26 10:52:38 +0100

    Inital setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+
+const style={
+  appContainer:`mx-w-[728px] mx-auto text-center`,
+  sectionContainer:`flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
+}
 
 const App=()=>{
   return (
-    <div>
-      App
+    <div className={style.appContainer}>
+      <div className={style.sectionContainer}>
+         <Navbar/>
+      </div>
     </div>
   )
 }
