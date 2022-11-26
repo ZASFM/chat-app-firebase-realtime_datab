9012dab3c88939fb5931d10de9d8169f62ba3e83
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-11-26 12:01:05 +0100

    Google logout set

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { auth } from "../firebase";
+import {signOut} from 'firebase/auth';
+
+const style={
+   button:`bg-gray-200 px-4 `,
+}
 
 const LogOut=()=>{
+
+   const singOutFromAccount=()=>{
+      signOut(auth);
+   }
+
    return (
-      <div>
+      <button className={style.button} onClick={singOutFromAccount}>
          Log out
-      </div>
+      </button>
    )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { auth } from "../firebase";
 import {useAuthState} from 'react-firebase-hooks/auth';
 import SignIn from "./Signin";
+import LogOut from "./Logout";
 
 const style={
    nav:`bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -13,7 +14,7 @@ const Navbar=()=>{
    return (
       <div className={style.nav}>
          <h1 className={style.heading}>Chat here:</h1>
-         <SignIn/>
+         {user?<LogOut/>:<SignIn/>}
       </div>
    )
 }
